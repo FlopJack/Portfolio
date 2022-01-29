f51aab7ef1581e74718606f923200073a3a89946
KML <flopjack.00@hotmail.com>
2022-01-29 20:01:42 +0100

    screugneugneu

--- client/src/Components/Headers/Television.jsx
+++ client/src/Components/Headers/Television.jsx
@@ -4,7 +4,10 @@ export default function Television() {
 	return (
 		<div className="divTv">
             <div id="screen"></div>
-			<img id="tv" src={television} alt='Logo' />
+			<map name="button">
+				<area coords='circle' coords='2300,1900,200' href="https://developer.mozilla.org/"/> 
+			</map>
+			<img usemap="#button" width={"80%"} height="80%" id="tv" src={television} alt='Logo' />
 		</div>
 	);
 }
