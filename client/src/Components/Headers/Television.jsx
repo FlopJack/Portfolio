e0eadebbfa599b83a14ca2571d2f5dc1f56542f8
KML <flopjack.00@hotmail.com>
2022-01-30 19:43:47 +0100

    Are good

--- client/src/Components/Headers/Television.jsx
+++ client/src/Components/Headers/Television.jsx
@@ -1,13 +1,13 @@
 import './Television.css';
-import television from '../Img/teleCalqueBon.png';
+import television from '../Img/teleCalqueBonReduit.png';
 export default function Television() {
 	return (
-		<div className="divTv">
-            <div id="screen"></div>
-			<map name="button">
-				<area coords='circle' coords='2300,1900,200' href="https://developer.mozilla.org/"/> 
-			</map>
-			<img usemap="#button" width={"80%"} height="80%" id="tv" src={television} alt='Logo' />
-		</div>
+<>
+		<img useMap="#imageMap"  id="tv" src={television} alt='Logo' />
+		<map name="imageMap">
+		<area target="" alt="" title="" href="" onClick={console.log("HEYOH")} coords="613,555,24"  shape="circle"/>
+
+		</map>
+		</>
 	);
 }
