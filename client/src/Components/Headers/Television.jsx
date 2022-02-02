b39fef8672fd3d05012cb3026f57785cb797e0be
FlopJack <39907800+FlopJack@users.noreply.github.com>
2022-02-02 18:01:27 +0100

    add svg maudit

--- client/src/Components/Headers/Television.jsx
+++ client/src/Components/Headers/Television.jsx
@@ -1,13 +1,25 @@
-import './Television.css';
-import television from '../Img/teleCalqueBonReduit.png';
-export default function Television() {
-	return (
-<>
-		<img useMap="#imageMap"  id="tv" src={television} alt='Logo' />
-		<map name="imageMap">
-		<area target="" alt="" title="" href="" onClick={console.log("HEYOH")} coords="613,555,24"  shape="circle"/>
+import "./Television.css";
+import television from "../Img/teleCalqueBonReduit.svg";
 
-		</map>
-		</>
-	);
+export default function Television() {
+  return (
+    <div>
+      <img useMap='#imageMap' id='tv' src={television} alt='Logo' />
+      <map name='imageMap'>
+        <area
+          target=''
+          alt=''
+          title=''
+          href=''
+          onClick={(e) => {
+            e.preventDefault();
+            console.log("HEYOH");
+          }}
+          id='tvButton'
+          coords='613,555,24'
+          shape='circle'
+        />
+      </map>
+    </div>
+  );
 }
