0228e145b61c49ef0112dbcf8a301daba6f13004
FlopJack <39907800+FlopJack@users.noreply.github.com>
2022-02-07 14:13:05 +0100

    add scss

--- client/src/Components/Headers/Television.jsx
+++ client/src/Components/Headers/Television.jsx
@@ -1,10 +1,26 @@
 import "./Television.css";
+
 import television from "../Img/teleCalqueBonReduit.png";
 import ImageMap from "image-map";
+import { useEffect } from "react";
 export default function Television() {
-  ImageMap("img[usemap]");
+  useEffect(() => {
+    ImageMap("img[usemap]");
+  }, []);
+  function turOnOffTV() {
+    let screen = document.getElementsByClassName("screen");
+    if (screen.item(0).classList.contains("screen-off")) {
+      screen[0].classList.remove("screen-off");
+      screen[0].classList.add("screen-on");
+    } else {
+      screen[0].classList.remove("screen-on");
+      screen[0].classList.add("screen-off");
+    }
+  }
+
   return (
-    <div>
+    <div id='divTv'>
+      <div className='screen screen-off'> Welcome</div>
       <img useMap='#imageMap' id='tv' src={television} alt='Logo' />
       <map name='imageMap'>
         <area
@@ -14,7 +30,7 @@ export default function Television() {
           href=''
           onClick={(e) => {
             e.preventDefault();
-            console.log("HEYOH");
+            turOnOffTV();
           }}
           id='tvButton'
           coords='613,555,24'
