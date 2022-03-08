b9ae14cab35c78dc87b221d568d54f4f33ebc423
FlopJack <39907800+FlopJack@users.noreply.github.com>
2022-03-08 23:31:01 +0100

    Tele good

--- client/src/Components/Tv/Television.jsx
+++ client/src/Components/Tv/Television.jsx
@@ -1,28 +1,35 @@
 import "../Style/Television.scss";
-import television from "../Img/teleCalqueBonReduit.png";
 
+import television from "../Img/teleCalqueBonReduit.png";
+import logoReact from "../Img/reactPng.png";
 import ImageMap from "image-map";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export default function Television() {
   useEffect(() => {
     ImageMap("img[usemap]");
   }, []);
+  const [reactLogo, setReactLogo] = useState(false);
   function turOnOffTV() {
     let screen = document.getElementsByClassName("screen");
     if (screen.item(0).classList.contains("screen-off")) {
       screen[0].classList.remove("screen-off");
       screen[0].classList.add("screen-on");
+      setReactLogo(true);
     } else {
       screen[0].classList.remove("screen-on");
       screen[0].classList.add("screen-off");
+      setReactLogo(false);
     }
   }
 
   return (
     <>
       <div id='divTV'>
-        <div className='screen screen-off'>
-          <div className='screenWelcome'>Welcome</div>
+        <div className='backscreen'>
+          <div className='screenWelcome'>
+            {reactLogo ? <img src={logoReact} className="imgLogo" width='50%' /> : <></>}
+          </div>
+          <div className='screen screen-off'></div>
           <img src={television} useMap='#imageMap' alt='' id='tv' />
         </div>
         <map name='imageMap'>
